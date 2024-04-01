ee2c9878a0657b7e35efb9db203e5005c101dc08
robemike <robemike1135@gmail.com>
2024-04-01 17:08:32 +0300

    updates index.js

--- index.js
+++ index.js
@@ -25,6 +25,59 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+// const animalSounds = {
+//   cow: "moo",
+//   horse: "neigh",
+//   sheep: "baa",
+//   pig: "oink",
+//   chicken: "cluck"
+
+// }
+
+const moo = "cow"
+const neigh = "horse"
+const baa = "sheep"
+const oink = "pig"
+const cluck = "chicken"
+
+const bessie = "cow"
+const dolly = "sheep"
+const babe = "pig"
+const little = "chicken"
+
+const blackAndWhite = "cow"
+const black = "sheep"
+const pink = "pig"
+
+const red = "red";
+const orange = "orange";
+const yellow = "yellow";
+const green = "green";
+const blue = "blue";
+const indigo = "indigo";
+const violet = "violet";
+
+const indg = "indigo";
+
+const r = "red";
+const o = "orange";
+const y = "yellow";
+const g = "green";
+const b = "blue";
+const i = "indigo";
+const v = "violet";
+
+const muppetName = "Miss Piggy";
+const color = 'pink';
+const song = 'Never Before, Never Again';
+const job = 'Cast member of The Muppet Show';
+const partner = 'Kermit';
+
+const song2 = "Moving Right Along";
+const song4 = "I Hope That Something Better Comes Along";
+const nestedJob = "Host of The Muppet Show";
+const nestedPartner = "Miss Piggy";
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
